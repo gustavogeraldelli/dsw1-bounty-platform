08ef0537f5f124e95e1c3bfe3b00cd650f5548a6
gustavo <geraldellig@gmail.com>
2026-05-26 16:07:44 -0300

    chore: project setup via Spring Initializr

---
+++ src/main/java/br/ufscar/dc/dsw/BugBountyPlatform/BugBountyPlatformApplication.java
@@ -0,0 +1,13 @@
+package br.ufscar.dc.dsw.BugBountyPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugBountyPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BugBountyPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ufscar/dc/dsw/BugBountyPlatform/BugBountyPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufscar.dc.dsw.BugBountyPlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugBountyPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
